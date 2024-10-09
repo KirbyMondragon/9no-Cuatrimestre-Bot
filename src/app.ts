3ce4474f959dc4a7e10ceb20bcdb0fab02d95c5b
El buen y fresco Kirby <167347481+KevinMondragonDev@users.noreply.github.com>
2024-10-08 20:59:04 -0600

    fix: limpiar la base de datos al iniciar para que no envie mensajes por fallos en la base de datos

--- src/app.ts
+++ src/app.ts
@@ -3,19 +3,26 @@ import { createBot, createProvider, createFlow, addKeyword, utils } from '@build
 import { MongoAdapter as Database } from '@builderbot/database-mongo'
 import { BaileysProvider as Provider } from '@builderbot/provider-baileys'
 import { welcomeFlow } from './flows/bienvenida';
+import { MongoClient }  from 'mongodb';
+import * as dotenv from 'dotenv';
+// Cargar las variables de entorno del archivo .env
+dotenv.config();
 
-const PORT = 3033;
-
+const PORT = process.env.PORT;
+const MONGOURI = process.env.MONGOURI;
+const MONGONAME= process.env.MONGONAME;
 
 const main = async () => {
     const adapterFlow = createFlow([welcomeFlow])
     
     const adapterProvider = createProvider(Provider)
-        const adapterDB = new Database({
-        dbUri: "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.5",
-        dbName: "DenisseChatbot",
+
+    const adapterDB = new Database({
+        dbUri: MONGOURI,
+        dbName: MONGONAME,
     })
 
+    
     const { handleCtx, httpServer } = await createBot({
         flow: adapterFlow,
         provider: adapterProvider,
@@ -26,4 +33,32 @@ const main = async () => {
     httpServer(+PORT)
 }
 
+
+async function deleteDatabase() {
+    const uri = MONGOURI;  // Cambia esto a la URL de tu instancia de MongoDB
+    const client = new MongoClient(uri);
+  
+    try {
+      // Conectar al cliente
+      await client.connect();
+  
+      // Nombre de la base de datos que quieres eliminar
+  
+      // Obtener la referencia de la base de datos
+      const db = client.db(MONGONAME);
+  
+      // Borrar la base de datos
+      await db.dropDatabase();
+  
+      console.warn(`La base de datos ${MONGONAME} esta lista para el bot`);
+    } catch (err) {
+      console.error("Error al configurar la base de datos:", err);
+    } finally {
+      // Cerrar la conexión
+      await client.close();
+    }
+  }
+  
+  
+deleteDatabase().catch(console.error);
 main()
